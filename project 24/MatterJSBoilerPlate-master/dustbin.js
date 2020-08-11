9894ca9d823b529c7a5fd8c9fba5337572b0e8f0
harneesh <67790133+harneesh@users.noreply.github.com>
2020-08-11 11:10:37 +0530

    Add files via upload

---
+++ project 24/MatterJSBoilerPlate-master/dustbin.js
@@ -0,0 +1,24 @@
+class Dustbin {
+    constructor(x, y, width, height) {
+      var options = {
+          'restitution':0,
+          'friction':2,
+          'density':100
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      push();
+      translate(pos.x, pos.y);
+      rectMode(CENTER);
+      fill("red");
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  };
+  
